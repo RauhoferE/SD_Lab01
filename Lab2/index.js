e9db5892e9af5da59fda0af015c168d4c4722512
RauhoferE <104609@fhwn.ac.at>
2020-10-23 13:34:08 +0200

    te

--- Lab2/index.js
+++ Lab2/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 let appInsights = require('applicationinsights');
 appInsights.setup("0bbba709-d776-48a2-9333-383f9ccb1479").start();
 const app = express()
-const port = 80
+const port = 3000
 
 app.get('/hw', (req, res) => {
   res.send('Hello World!')
